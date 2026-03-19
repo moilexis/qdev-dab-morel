e2bfb9363758a9340f474bb9d6a76179da225074
Alexis Morel <22501205@me.uvsq.fr>
2026-03-19 11:49:28 +0100

    correction des erreurs d'arguments

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,9 +4,11 @@ from dab.account import *
 class DABUnitTests(unittest.TestCase):
   def test_checkWithdrawal(self):
     # cas de test n°1:
-    self.assertEqual(checkWithdrawal(20,350), True)
+    self.assertEqual(checkWithdrawal(350,20), True)
     # cas de test n°2 :
-    self.assertEqual(checkWithdrawal(490, 350), False)
+    self.assertEqual(checkWithdrawal(350, 490), False)
+    # cas de test n°2 :
+    self.assertEqual(checkWithdrawal(3050,500 ), False)
 
 
 if __name__ == '__main__':
