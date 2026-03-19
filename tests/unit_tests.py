9076bde12aa04c0b1e1cbef287ac543a5b4c069f
Alexis Morel <22501205@me.uvsq.fr>
2026-03-19 11:47:25 +0100

    création du test de cas 2

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -5,6 +5,8 @@ class DABUnitTests(unittest.TestCase):
   def test_checkWithdrawal(self):
     # cas de test n°1:
     self.assertEqual(checkWithdrawal(20,350), True)
+    # cas de test n°2 :
+    self.assertEqual(checkWithdrawal(490, 350), False)
 
 
 if __name__ == '__main__':
