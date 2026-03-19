feba508358a3e3c27a4185a4c6279dacd8f14f68
Alexis Morel <22501205@me.uvsq.fr>
2026-03-19 11:46:33 +0100

    création du test de cas 1

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -3,7 +3,9 @@ from dab.account import *
 
 class DABUnitTests(unittest.TestCase):
   def test_checkWithdrawal(self):
-    self.assertEqual(True, False)
+    # cas de test n°1:
+    self.assertEqual(checkWithdrawal(20,350), True)
+
 
 if __name__ == '__main__':
   unittest.main()
